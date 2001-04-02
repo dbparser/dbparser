e6b78e72ea2786fe149d0d6f3b91e2ef2cc0593f
dbikel <dbikel@b85a8ef5-4420-0410-ac20-a3093ec9fedb>
2001-04-02 21:33:40 +0000

    Changed so that SLNode now no longer extends Pair, but directly contains the appropriate two data members.

--- src/danbikel/util/SLNode.java
+++ src/danbikel/util/SLNode.java
@@ -5,28 +5,32 @@ import java.util.*;
 /**
  * Represents a node in a singly-linked list.
  */
-public class SLNode extends Pair {
-  public SLNode(Object data, Object next) {
-    super(data, next);
+public class SLNode {
+  private Object data;
+  private SLNode next;
+
+  public SLNode(Object data, SLNode next) {
+    this.data = data;
+    this.next = next;
   }
 
   /**
    * Returns the data associated with this node of the list.
    */
-  public Object data() { return first; }
+  public Object data() { return data; }
 
   /**
    * Returns the next node of this list.
    */
-  public SLNode next() { return (SLNode)second; }
+  public SLNode next() { return next; }
 
   /**
    * Sets the next node of this list.
    *
    * @param newNext the new node to be inserted after this node
    */
-  private void setNext(SLNode newNext) {
-    second = newNext;
+  private void setNext(SLNode next) {
+    this.next = next;
   }
 
   /**
@@ -82,5 +86,44 @@ public class SLNode extends Pair {
         curr = curr.next();
       }
     };
+  }
+
+  public String toString() {
+    StringBuffer sb = new StringBuffer();
+    sb.append("(");
+    SLNode curr = this;
+    while (curr != null) {
+      sb.append(curr.data);
+      curr = curr.next;
+    }
+    sb.append(")");
+    return sb.toString();
+  }
+
+  public int hashCode() {
+    int code = 0;
+    SLNode curr = this;
+    while (curr != null) {
+      code = (code * 31) + (curr.data == null ? 0 : curr.data.hashCode());
+      curr = curr.next;
+    }
+    return code;
+  }
+
+  public boolean equals(Object obj) {
+    if (!(obj instanceof SLNode))
+      return false;
+    SLNode other = (SLNode)obj;
+    SLNode curr = this;
+    while (curr != null && other != null) {
+      if ((curr.data == null && other.data != null) ||
+          (curr.data != null && other.data == null))
+        return false;
+      if (curr.data != null && curr.data.equals(other.data) == false)
+        return false;
+      curr = curr.next;
+      other = other.next;
+    }
+    return curr == null && other == null;
   }
 }

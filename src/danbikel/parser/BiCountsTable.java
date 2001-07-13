caafd64020c570346c54ef53d36f1433ef79bc40
dbikel <dbikel@b85a8ef5-4420-0410-ac20-a3093ec9fedb>
2001-07-13 16:08:38 +0000

    Allows arbitrary objects to be associated with two different integer counts.

---
+++ src/danbikel/parser/BiCountsTable.java
@@ -0,0 +1,77 @@
+package danbikel.parser;
+
+import danbikel.util.*;
+import danbikel.lisp.*;
+import java.io.*;
+import java.util.*;
+
+/**
+ * Provides a mapping between objects and integer counts that may be
+ * incremented or decremented.
+ */
+public class BiCountsTable extends danbikel.util.HashMapTwoInts {
+  /**
+   * Constructs an empty <code>CountsTable</code>.
+   */
+  public BiCountsTable() {
+    super();
+  }
+
+  /**
+   * Constructs an empty <code>CountsTable</code> with the specified initial
+   * number of hash buckets.
+   *
+   * @param initialCapacity the number of hash buckets that this object
+   * will initially have
+   */
+  public BiCountsTable(int initialCapacity) {
+    super(initialCapacity);
+  }
+  /**
+   * Constructs an empty <code>CountsTable</code> with the specified initial
+   * number of hash buckets and the specified load factor.  If the load
+   * factor, which is average number of items per bucket, is exceeded
+   * at runtime, the number of buckets is roughly doubled and the entire
+   * map is re-hashed, as implemented by the parent class, {@link HashMap}.
+   *
+   * @param initialCapacity the number of hash buckets that this object
+   * will initially have
+   * @param loadFactor the load factor of this <code>HashMap</code> object
+   */
+  public BiCountsTable(int initialCapacity, float loadFactor) {
+    super(initialCapacity, loadFactor);
+  }
+
+  public void add(Object key, int index) {
+    add(key, index, 1);
+  }
+
+  public int count(Object key, int index) {
+    /*
+    if (index < 0 || index > 1)
+      throw new IllegalArgumentException();
+    */
+    MapToPrimitive.Entry e = getEntry(key);
+    return (e == null ? 0 : e.getIntValue(index));
+  }
+
+  /**
+   * Outputs all the mappings of this map in as S-expressions of the form
+   * <pre>(name key value)</pre>
+   */
+  public void output(String eventName, Writer writer) throws IOException {
+    Iterator keys = keySet().iterator();
+    while (keys.hasNext()) {
+      Object o = keys.next();
+      writer.write("(");
+      writer.write(eventName);
+      writer.write(" ");
+      writer.write(String.valueOf(o));
+      writer.write(" ");
+      writer.write(String.valueOf(count(o, 0)));
+      writer.write(" ");
+      writer.write(String.valueOf(count(o, 1)));
+      writer.write(")\n");
+    }
+  }
+}

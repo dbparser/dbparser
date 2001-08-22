16de84f71694d3e218d5750853da4e4fee0c5650
dbikel <dbikel@b85a8ef5-4420-0410-ac20-a3093ec9fedb>
2001-08-22 18:33:46 +0000

    Added methods to analyze gold-standard parses w.r.t. chart. Fixed some bugs related to these debugging methods.

--- src/danbikel/parser/util/DebugChart.java
+++ src/danbikel/parser/util/DebugChart.java
@@ -1,13 +1,17 @@
 package danbikel.parser.util;
 
+import danbikel.util.*;
 import danbikel.lisp.*;
 import danbikel.parser.*;
 import java.io.*;
 import java.util.*;
 
 public class DebugChart {
-  // data member
+  // data members
   private static Symbol traceTag = Language.training().traceTag();
+
+  private static HashSet best = new HashSet();
+  private static SortedSet sortedItems = new TreeSet();
 
   private DebugChart() {}
 
@@ -36,14 +40,19 @@ public class DebugChart {
       ObjectInputStream ois =
 	new ObjectInputStream(new FileInputStream(chartFilename));
       Chart chart = (Chart)ois.readObject();
+      CKYItem topRankedItem = (CKYItem)ois.readObject();
+      SexpList sentence = (SexpList)ois.readObject();
       SexpList origWords = (SexpList)ois.readObject();
       Sexp newGoldTree = Language.training().preProcess(goldTree);
+      String downcaseWordsStr = Settings.get(Settings.downcaseWords);
+      boolean downcaseWords = Boolean.valueOf(downcaseWordsStr).booleanValue();
+      if (downcaseWords)
+	downcaseWords(newGoldTree);
+      replaceWords(downcaseWords, newGoldTree, sentence);
       System.err.println(Util.prettyPrint(newGoldTree));
       HeadTreeNode headTree = new HeadTreeNode(newGoldTree);
-      String downcaseWordsStr = Settings.get(Settings.downcaseWords);
-      if (Boolean.valueOf(downcaseWordsStr).booleanValue())
-	downcaseWords(headTree);
-      findConstituents(chart, headTree);
+      Set best = collectBest(topRankedItem);
+      findConstituents(chart, best, headTree);
     }
     catch (FileNotFoundException fnfe) {
       System.err.println(fnfe);
@@ -75,17 +84,150 @@ public class DebugChart {
    * <tt>combined</tt> directory of the Penn Treebank, except with its
    * outer parentheses removed
    */
-  public static void findConstituents(Chart chart, Sexp goldTree) {
+  public static void findConstituents(boolean downcaseWords,
+                                      Chart chart,
+                                      CKYItem topRankedItem,
+                                      SexpList sentence,
+                                      Sexp goldTree) {
+    findConstituents("", downcaseWords, chart, topRankedItem,
+                     sentence, goldTree);
+  }
+
+  /**
+   * Prints out to <code>System.err</code> which constituents of the specified
+   * gold-standard parse tree were found by the parser, according to the
+   * specified chart.  This method is intended to be used for on-line parser
+   * debugging (that is, debugging during a decoding run).  The only functional
+   * difference between this method and {@link #findConstituents(String,Sexp)}
+   * is that this method does not print out the original words of the sentence,
+   * as that can be separately accomplished during decoding by setting
+   * <code>Decoder.debugInit</code> to <code>true</code>.
+   *
+   * @param prefix the prefix string to be output before other information
+   * on lines that don't begin with a tab character
+   * @param chart the chart of the parser, after processing the sentence
+   * to be analyzed
+   * @param goldTree the gold-standard parse tree, as found in the original
+   * <tt>combined</tt> directory of the Penn Treebank, except with its
+   * outer parentheses removed
+   */
+  public static void findConstituents(String prefix,
+                                      boolean downcaseWords,
+				      Chart chart,
+                                      CKYItem topRankedItem,
+                                      SexpList sentence,
+                                      Sexp goldTree) {
     Sexp newGoldTree = Language.training().preProcess(goldTree);
+    if (downcaseWords)
+      downcaseWords(newGoldTree);
+    replaceWords(downcaseWords, newGoldTree, sentence);
     HeadTreeNode headTree = new HeadTreeNode(newGoldTree);
-    String downcaseWordsStr = Settings.get(Settings.downcaseWords);
-    if (Boolean.valueOf(downcaseWordsStr).booleanValue())
-      downcaseWords(headTree);
-    findConstituents(chart, headTree);
+    Set best = collectBest(topRankedItem);
+    findConstituents(prefix, chart, best, headTree);
+  }
+
+  public static void replaceWords(boolean downcaseWords,
+                                  Sexp tree, SexpList sentence) {
+    replaceWords(downcaseWords, tree, sentence, 0);
+  }
+
+  private static int replaceWords(boolean downcaseWords,
+                                  Sexp tree, SexpList sentence, int wordIdx) {
+    Treebank treebank = Language.treebank();
+    if (treebank.isPreterminal(tree))
+      return wordIdx;
+    if (tree.isList()) {
+      SexpList treeList = tree.list();
+      int treeListLen = treeList.length();
+      for (int i = 1; i < treeListLen; i++) {
+        Sexp currChild = treeList.get(i);
+        if (treebank.isPreterminal(currChild)) {
+          Sexp sentenceElt = sentence.get(wordIdx);
+          boolean isUnknownWord = sentenceElt.isList();
+          if (isUnknownWord) {
+            Word word = Language.treebank().makeWord(currChild);
+            Symbol sentWord = sentenceElt.list().symbolAt(0);
+            if (downcaseWords)
+              sentWord = Symbol.get(sentWord.toString().toLowerCase());
+            Symbol wordFeature = sentenceElt.list().symbolAt(1);
+            if (sentWord == word.word()) {
+              word.setWord(wordFeature);
+              treeList.set(i, treebank.constructPreterminal(word));
+            }
+            else
+              System.err.println("chart debug error: didn't replace " +
+                                 word.word() + " with feature from " +
+                                 sentenceElt);
+          }
+          wordIdx++;
+        }
+        else
+          wordIdx = replaceWords(downcaseWords, currChild, sentence, wordIdx);
+      }
+    }
+    return wordIdx;
+  }
+
+  public static void printBestDerivationStats(String prefix,
+					      Chart chart,
+					      int sentLen,
+					      Symbol topSym,
+					      double nonTopHighestLogProb,
+					      CKYItem bestDerivationItem) {
+    if (bestDerivationItem == null)
+      return;
+    // first, get distance (as a log prob addend, or probability factor)
+    // of the current item of the best derivation to the top-ranked item
+    // for the current item's span
+    CKYItem bdi = bestDerivationItem;
+    int start = bdi.start();
+    int end = bdi.end();
+    boolean nonTopSentSpanItem =
+      (bdi.label() != topSym) && ((end + 1 - start) == sentLen);
+    double highestLogProb =
+      nonTopSentSpanItem ? nonTopHighestLogProb : chart.getTopLogProb(start, end);
+    double distance = highestLogProb - bdi.logProb();
+    // next, get the current best-derivation item's rank among all items
+    // covering its span
+    int rank = -1;
+    if (bdi == chart.getTopItem(start, end))
+      rank = 0;
+    else {
+      sortedItems.clear();
+      // add all items to sorted items
+      Iterator it = chart.get(start, end);
+      while (it.hasNext())
+	sortedItems.add(it.next());
+      rank = sortedItems.tailSet(bdi).size() - 1;
+    }
+
+    System.err.println(prefix + itemToString(bdi) + ", span=" +
+		       (end + 1 -start) +
+		       ", dist=" + distance + ", rank=" + rank + " of " +
+		       chart.numItems(start, end));
+
+    // recurse on head child
+    printBestDerivationStats(prefix, chart, sentLen, topSym,
+			     nonTopHighestLogProb, bdi.headChild());
+    // recurse on left children
+    for (SLNode lc = bdi.leftChildren(); lc != null; lc = lc.next())
+      printBestDerivationStats(prefix, chart, sentLen, topSym,
+			       nonTopHighestLogProb, (CKYItem)lc.data());
+    // recurse on right children
+    for (SLNode rc = bdi.rightChildren(); rc != null; rc = rc.next())
+      printBestDerivationStats(prefix, chart, sentLen, topSym,
+			       nonTopHighestLogProb, (CKYItem)rc.data());
   }
 
   // helper methods
-  public static void findConstituents(Chart chart, HeadTreeNode tree) {
+  public static void findConstituents(Chart chart, Set best,
+                                      HeadTreeNode tree) {
+    findConstituents("", chart, best, tree);
+  }
+
+  public static void findConstituents(String prefix,
+				      Chart chart, Set best,
+                                      HeadTreeNode tree) {
     if (!tree.isPreterminal()) {
       int start = tree.leftIdx();
       // head tree nodes specify right index as index of rightmost word PLUS 1,
@@ -106,7 +248,8 @@ public class DebugChart {
 	    unlexicalizedFoundNoStop = item;
 	  if (item.headWord().equals(tree.headWord())) {
 	    if (item.stop()) {
-	      System.err.println("found " + itemToString(item));
+	      System.err.println(prefix + "found " +
+                                 itemToString(item, best));
 	      found = item;
 	      break;
 	    }
@@ -118,19 +261,23 @@ public class DebugChart {
        }
 
       if (found == null) {
-	System.err.print("didn't find " + headTreeNodeToString(tree));
+	System.err.print(prefix + "didn't find " +
+			 headTreeNodeToString(tree));
 	boolean foundOther =
 	  foundNoStop != null ||
 	  unlexicalizedFound != null || unlexicalizedFoundNoStop != null;
 	if (foundOther) {
 	  System.err.println(" but found:" +
 			     (unlexicalizedFound != null ?
-			      "\n\t" + itemToString(unlexicalizedFound) : "") +
+			      "\n\t" +
+                              itemToString(unlexicalizedFound, best) : "") +
 			     (foundNoStop != null ?
-			      "\n\t" + itemToString(foundNoStop) : "") +
+			      "\n\t" +
+                              itemToString(foundNoStop, best) : "") +
 			     (unlexicalizedFound == null &&
 			      unlexicalizedFoundNoStop != null ?
-			      "\n\t" + itemToString(unlexicalizedFoundNoStop) :
+			      "\n\t" +
+                              itemToString(unlexicalizedFoundNoStop, best) :
 			      ""));
 	}
 	else
@@ -138,15 +285,41 @@ public class DebugChart {
       }
 
       // recurse on head child
-      findConstituents(chart, tree.headChild());
+      findConstituents(prefix, chart, best, tree.headChild());
       // recurse on pre- and post-mods
       it = tree.preMods().iterator();
       while (it.hasNext())
-	findConstituents(chart, (HeadTreeNode)it.next());
+	findConstituents(prefix, chart, best, (HeadTreeNode)it.next());
       it = tree.postMods().iterator();
       while (it.hasNext())
-	findConstituents(chart, (HeadTreeNode)it.next());
+	findConstituents(prefix, chart, best, (HeadTreeNode)it.next());
     }
+  }
+
+  public static Set collectBest(CKYItem topRanked) {
+    best.clear();
+    if (topRanked == null)
+      return best;
+    return collectBest(topRanked, best);
+  }
+
+  private static Set collectBest(CKYItem curr, Set best) {
+    if (curr.isPreterminal()) {
+      best.add(curr);
+    }
+    else {
+      // collect this item
+      best.add(curr);
+      // recurse on head child
+      collectBest(curr.headChild(), best);
+      // recurse on left children
+      for (SLNode lc = curr.leftChildren(); lc != null; lc = lc.next())
+        collectBest((CKYItem)lc.data(), best);
+      // recurse on right children
+      for (SLNode rc = curr.rightChildren(); rc != null; rc = rc.next())
+        collectBest((CKYItem)rc.data(), best);
+    }
+    return best;
   }
 
   /**
@@ -170,7 +343,21 @@ public class DebugChart {
 	     node.headChild().label() +
 	     "]") +
 	    "]");
-	    
+
+  }
+
+  public static String itemToString(CKYItem item, Set best) {
+    return ("[" + item.start() + "," + item.end() + "," +
+	    item.label() + item.headWord() + ",stop=" +
+	    (item.stop() ? "t" : "f") + ", best=" +
+            (best.contains(item) ? "t" : "f") + ", " +
+	    (item.isPreterminal() ? "[null]" :
+	     "[" +
+	     item.headChild().start() + "," +
+	     item.headChild().end() + "," +
+	     item.headChild().label() +
+	     "]") +
+	    "]");
   }
 
   public static String itemToString(CKYItem item) {
@@ -184,6 +371,27 @@ public class DebugChart {
 	     item.headChild().label() +
 	     "]") +
 	    "]");
+  }
+
+  public static void downcaseWords(Sexp tree) {
+    Treebank treebank = Language.treebank();
+    if (treebank.isPreterminal(tree))
+      return;
+    if (tree.isList()) {
+      SexpList treeList = tree.list();
+      int treeListLen = treeList.length();
+      for (int i = 1; i < treeListLen; i++) {
+        Sexp currChild = treeList.get(i);
+        if (treebank.isPreterminal(currChild)) {
+          Word word = treebank.makeWord(currChild);
+          word.setWord(Symbol.add(word.word().toString().toLowerCase()));
+          treeList.set(i, treebank.constructPreterminal(word));
+        }
+        else {
+          downcaseWords(currChild);
+        }
+      }
+    }
   }
 
   public static void downcaseWords(HeadTreeNode tree) {
@@ -220,7 +428,7 @@ public class DebugChart {
       return tree;
     if (tree.isList()) {
       SexpList treeList = tree.list();
-      for (int i = 0; i < treeList.length(); i++) {
+      for (int i = 1; i < treeList.length(); i++) {
 	Sexp currChild = treeList.get(i);
 	if (Language.treebank().isPreterminal(currChild)) {
 	  Word treeWord = Language.treebank().makeWord(currChild);
@@ -248,7 +456,7 @@ public class DebugChart {
       return tree;
     if (tree.isList()) {
       SexpList treeList = tree.list();
-      for (int i = 0; i < treeList.length(); i++) {
+      for (int i = 1; i < treeList.length(); i++) {
 	Sexp currChild = treeList.get(i);
 	if (!Language.treebank().isPreterminal(tree) &&
 	    currChild.isList() && currChild.list().length() == 1)

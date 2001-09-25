f5b51c9cb8d4beb33c0edf78b43c521d00be6d57
dbikel <dbikel@b85a8ef5-4420-0410-ac20-a3093ec9fedb>
2001-09-25 22:23:00 +0000

    Space- and time-efficient implementor of the FixedSizeList interface when the fixed size is 1.

---
+++ src/danbikel/util/FixedSizeSingletonList.java
@@ -0,0 +1,47 @@
+package danbikel.util;
+
+import java.util.*;
+
+public class FixedSizeSingletonList extends AbstractFixedSizeList {
+  protected Object obj;
+
+  public FixedSizeSingletonList(int size) {
+    super(size);
+  }
+
+  public FixedSizeSingletonList(Collection c) {
+    super(c);
+  }
+
+  protected void initialize(int size) {
+    if (size != 1)
+      throw new IllegalArgumentException();
+  }
+
+  public int size() { return 1; }
+
+  public Object get(int index) {
+    /*
+    if (index != 1)
+      throw new IndexOutOfBoundsException();
+    */
+    return obj;
+  }
+
+  public Object set(int index, Object obj) {
+    /*
+    if (index != 1)
+      throw new IndexOutOfBoundsException();
+    */
+    Object old = obj;
+    this.obj = obj;
+    return old;
+  }
+
+  public int hashCode() { return obj == null ? 0 : obj.hashCode(); }
+
+  public boolean shift(Object obj) {
+    this.obj = obj;
+    return true;
+  }
+}

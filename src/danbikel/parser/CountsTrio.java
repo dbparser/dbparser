d9a9fc942b4905835a0b7723d2736a8664a5cda6
dbikel <dbikel@b85a8ef5-4420-0410-ac20-a3093ec9fedb>
2001-09-25 21:22:05 +0000

    Updated class-level javadoc comment.

--- src/danbikel/parser/CountsTrio.java
+++ src/danbikel/parser/CountsTrio.java
@@ -3,9 +3,14 @@ package danbikel.parser;
 import java.io.Serializable;
 
 /**
- * Class for grouping the three <code>CountsTable</code>
- * objects necessary for counting transitions, histories and
- * unique transitions (or <i>diversity</i> counts for the history events).
+ * Class for grouping the three counts tables necessary for counting
+ * transitions, histories and unique transitions (or <i>diversity</i> counts
+ * for the history events).
+ * <p>
+ * <b>Implementation note</b>: The three tables are stored internally as
+ * two objects, a <code>CountsTable</code> and a <code>BiCountsTable</code>
+ * object.  These two objects are available from the {@link #transition()}
+ * and {@link #history()} accessor methods, respectively.
  */
 public class CountsTrio implements Serializable {
   // public constants

1b2dd60f06055f738103839753255842a8eba060
dbikel <dbikel@b85a8ef5-4420-0410-ac20-a3093ec9fedb>
2002-06-25 17:35:53 +0000

    Modified main method so that it now takes -tags option, which indicates to spit out sentences of the form ((word1 (tag1)) ... ).

--- src/danbikel/parser/util/ParseToSentence.java
+++ src/danbikel/parser/util/ParseToSentence.java
@@ -14,28 +14,65 @@ public class ParseToSentence {
 
   private ParseToSentence() {}
 
+  private static String[] usageMsg = {
+    "usage: [-v|-help|-usage] [-tags] [filename]",
+    "where",
+    "\t-v|-help|-usage: prints out this message",
+    "\t-tags: indicates to spit out one S-expression per word, of the form",
+    "\t\t(word (tag))",
+    "\tfilename is the file to be processed (standard input is assumed if",
+    "\t\tthis argument is \"-\" or is not present)"
+  };
+
+  private static void usage() {
+    for (int i = 0; i < usageMsg.length; i++)
+      System.err.println(usageMsg[i]);
+  }
+
   /**
    * Reads parse trees either from standard input or a specified file,
    * converting them to sentences and printing those sentences on standard
    * output.
-   * <pre>usage: [filename]</pre>
-   * where standard input is assumed if no argument is present.
+   * <pre>
+   * usage: [-v|-help|-usage] [-tags] [filename]
+   *         -v|-help|-usage: prints out this message
+   *         -tags: indicates to spit out one S-expression per word, of the form
+   *                 (word (tag))
+   *         filename is the file to be processed (standard input is assumed if
+   *                 this argument is "-" or is not present)
+   * </pre>
    */
   public static void main(String[] args) {
     InputStream inStream = System.in;
-    if (args.length > 0) {
+    boolean tags = false;
+    String inFile = null;
+    for (int i = 0; i < args.length; i++) {
+      if (args[i].equals("-help") || args[i].equals("-usage") ||
+          args[i].equals("-v")) {
+        usage();
+        return;
+      }
+      else if (args[i].equals("-tags"))
+        tags = true;
+      else if (!inFile.equals("-"))
+        inFile = args[i];
+    }
+    if (inFile != null) {
       try {
-	inStream = new FileInputStream(args[0]);
+	inStream = new FileInputStream(inFile);
       } catch (FileNotFoundException fnfe) {
 	System.err.println(fnfe);
 	System.exit(-1);
       }
     }
     try {
-      SexpTokenizer tok = new SexpTokenizer(inStream, Language.encoding(), bufSize);
+      SexpTokenizer tok =
+        new SexpTokenizer(inStream, Language.encoding(), bufSize);
       Sexp curr = null;
       while ((curr = Sexp.read(tok)) != null)
-        System.out.println(Util.collectLeaves(curr));
+        System.out.println(tags ?
+                           Util.collectTaggedWords(curr) :
+                           Util.collectLeaves(curr));
     }
     catch (Exception e) {
       System.out.println(e);

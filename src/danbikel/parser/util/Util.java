5f30458fbe380acaad34a431d3756be338bc00e8
dbikel <dbikel@b85a8ef5-4420-0410-ac20-a3093ec9fedb>
2002-06-25 17:58:10 +0000

    Changed collectLeaves(Sexp tree, SexpList leaves) to be collectLeaves(Sexp tree, SexpList leaves, boolean withTags). If third parameter is false, then the method, as before, simply fills the second parameter with the words (Symbol objects) that form the leaves of the specified tree.  If true, then the method fills in the leaves list with lists of the form (word (tag)).

--- src/danbikel/parser/util/Util.java
+++ src/danbikel/parser/util/Util.java
@@ -15,30 +15,61 @@ public class Util {
   /**
    * Returns a <code>SexpList</code> that contains all the leaves of the
    * specified parse tree.
+   *
+   * @param tree the tree from which to collect leaves (words)
+   * @return a list of the words contained in the specified tree
    */
   public static Sexp collectLeaves(Sexp tree) {
     SexpList leaves = new SexpList();
-    collectLeaves(tree, leaves);
+    collectLeaves(tree, leaves, false);
     return leaves;
   }
-  private static void collectLeaves(Sexp tree, SexpList leaves) {
+
+  /**
+   * Returns a <code>SexpList</code> that contains all the words of the
+   * specified parse tree as well as their part of speech tags, where each
+   * word is its own <code>SexpList</code> of the form <tt>(word (tag))</tt>.
+   *
+   * @param tree the tree from which to collect tagged words
+   * @return a list of tagged words from the specified tree
+   */
+  public static Sexp collectTaggedWords(Sexp tree) {
+    SexpList taggedWords = new SexpList();
+    collectLeaves(tree, taggedWords, true);
+    return taggedWords;
+  }
+
+  private static void collectLeaves(Sexp tree, SexpList leaves,
+                                    boolean withTags) {
     if (Language.treebank().isPreterminal(tree)) {
       if (!Language.treebank().isNullElementPreterminal(tree)) {
         Word word = Language.treebank().makeWord(tree);
-        leaves.add(word.word());
+        Sexp leaf = null;
+        if (withTags) {
+          SexpList tagList = new SexpList(1).add(word.tag());
+          leaf = new SexpList(2).add(word.word()).add(tagList);
+        }
+        else
+          leaf = word.word();
+
+        leaves.add(leaf);
       }
     }
     else if (tree.isList()) {
       SexpList treeList = tree.list();
       int treeListLen = treeList.length();
       for (int i = 0; i < treeListLen; i++) {
-        collectLeaves(treeList.get(i), leaves);
+        collectLeaves(treeList.get(i), leaves, withTags);
       }
     }
   }
 
   /**
    * Returns a string containing the pretty-printed version of the specified
+   * parse tree.
+   *
+   * @param tree the tree to pretty-print
+   * @return a string containing the pretty-printed version of the specified
    * parse tree.
    */
   public static String prettyPrint(Sexp tree) {

f01c8fe0ff0c9947f9949f7cd1db6926eb2c7230
dbikel <dbikel@b85a8ef5-4420-0410-ac20-a3093ec9fedb>
2002-06-26 17:58:53 +0000

    * Added the necessary side element to all histories constructed by getHistory method (see comment below regarding the removeHistory method).
    * Overrode doCleanup() method to return true.
    
    * Overrode the default, do-nothing implementation of the
    removeHistory(int,Event) method.  Here is the comment explaining why.
    The trainer passes ModifierEvent objects representing modifiers on both sides
    of the head, even though there are separate models for each side.  This allows
    the final back-off level, p(w | t), to use information gathered from both
    sides.  However, it means that after all counts have been derived, there are
    twice as many entries in the counts tables for the back-off levels 0 and 1,
    since the model for, say, the left side will have stored counts for the right
    side as well (the counts are stored separately by explicitly including a "side"
    field in the events being counted).  This method allows for the removal of
    these "unnecessary" counts, which will never be used when decoding.
    
    * Updated copy method to include crucial additionalData field, which indicates
    which side of the head this probability structure is modeling.

--- src/danbikel/parser/ModWordModelStructure2.java
+++ src/danbikel/parser/ModWordModelStructure2.java
@@ -12,7 +12,7 @@ public class ModWordModelStructure2 extends ProbabilityStructure {
     super();
   }
 
-  public int maxEventComponents() { return 9; }
+  public int maxEventComponents() { return 10; }
   public int numLevels() { return 3; }
 
   public Event getHistory(TrainerEvent trainerEvent, int backOffLevel) {
@@ -21,36 +21,43 @@ public class ModWordModelStructure2 extends ProbabilityStructure {
     if (modEvent.parent() == baseNP)
       return getBaseNPHistory(modEvent, backOffLevel);
 
+    Symbol side = Constants.sideToSym(modEvent.side());
+
     MutableEvent hist = historiesWithSubcats[backOffLevel];
     hist.clear();
     Symbol verbInterveningSym =
       Constants.booleanToSym(modEvent.verbIntervening());
     Symbol mappedPrevModSym =
       Collins.mapPrevMod(modEvent.previousMods().symbolAt(0));
+    Symbol parent =
+      Language.training.removeArgAugmentation(modEvent.parent());
 
     switch (backOffLevel) {
     case 0:
-      // for p(w_i | M(t)_i, P, H, w, t, verbIntervening, map(M_i-1),  subcat)
+      // for p(w_i | M(t)_i, P, H, w, t, verbIntervening, map(M_i-1),  subcat,
+      //             side)
       hist.add(0, Language.training.removeGapAugmentation(modEvent.modifier()));
       hist.add(0, modEvent.modHeadWord().tag());
-      hist.add(0, Language.training.removeGapAugmentation(modEvent.parent()));
+      hist.add(0, parent);
       hist.add(0, Language.training.removeGapAugmentation(modEvent.head()));
       hist.add(0, modEvent.headWord().word());
       hist.add(0, modEvent.headWord().tag());
       hist.add(0, verbInterveningSym);
       hist.add(0, mappedPrevModSym);
       hist.add(1, modEvent.subcat());
+      hist.add(0, side);
       break;
     case 1:
-      // for p(w_i | M(t)_i, P, H, t, verbIntervening, map(M_i-1), subcat)
+      // for p(w_i | M(t)_i, P, H, t, verbIntervening, map(M_i-1), subcat, side)
       hist.add(0, Language.training.removeGapAugmentation(modEvent.modifier()));
       hist.add(0, modEvent.modHeadWord().tag());
-      hist.add(0, Language.training.removeGapAugmentation(modEvent.parent()));
+      hist.add(0, parent);
       hist.add(0, Language.training.removeGapAugmentation(modEvent.head()));
       hist.add(0, modEvent.headWord().tag());
       hist.add(0, verbInterveningSym);
       hist.add(0, mappedPrevModSym);
       hist.add(1, modEvent.subcat());
+      hist.add(0, side);
       break;
     case 2:
       // for p(w_i | t_i)
@@ -64,6 +71,9 @@ public class ModWordModelStructure2 extends ProbabilityStructure {
 
   private Event getBaseNPHistory(ModifierEvent modEvent, int backOffLevel) {
     MutableEvent hist = histories[backOffLevel];
+
+    Symbol side = Constants.sideToSym(modEvent.side());
+
     Symbol prevModLabel =
       (modEvent.previousMods().get(0) == startSym ?
        modEvent.head() : modEvent.previousMods().symbolAt(0));
@@ -73,30 +83,33 @@ public class ModWordModelStructure2 extends ProbabilityStructure {
     hist.clear();
     switch (backOffLevel) {
     case 0:
-      // for p(w_i | M(t)_i, P, M(w,t)_i-1)
+      // for p(w_i | M(t)_i, P, M(w,t)_i-1, side)
       hist.add(Language.training.removeGapAugmentation(modEvent.modifier()));
       hist.add(modEvent.modHeadWord().tag());
       hist.add(Language.training.removeGapAugmentation(modEvent.parent()));
       hist.add(prevModLabel);
       hist.add(prevModWord.word());
       hist.add(prevModWord.tag());
+      hist.add(side);
       break;
       /*
     case 1:
-      // for p(w_i | M(t)_i, P, M(t)_i-1)
+      // for p(w_i | M(t)_i, P, M(t)_i-1, side)
       hist.add(Language.training.removeGapAugmentation(modEvent.modifier()));
       hist.add(modEvent.modHeadWord().tag());
       hist.add(Language.training.removeGapAugmentation(modEvent.parent()));
       hist.add(prevModLabel);
       hist.add(prevModWord.tag());
+      hist.add(side);
       break;
       */
     case 1:
-      // for p(w_i | M(t)_i, P, M_i-1)
+      // for p(w_i | M(t)_i, P, M_i-1, side)
       hist.add(Language.training.removeGapAugmentation(modEvent.modifier()));
       hist.add(modEvent.modHeadWord().tag());
       hist.add(Language.training.removeGapAugmentation(modEvent.parent()));
       hist.add(prevModLabel);
+      hist.add(side);
       break;
     case 2:
       // for p(w_i | t_i)
@@ -109,11 +122,52 @@ public class ModWordModelStructure2 extends ProbabilityStructure {
   public Event getFuture(TrainerEvent trainerEvent, int backOffLevel) {
     MutableEvent future = futures[backOffLevel];
     future.clear();
-    future.add(((ModifierEvent)trainerEvent).modHeadWord().word());
+    Word modHead = ((ModifierEvent)trainerEvent).modHeadWord();
+    Symbol word =
+      modHead.features() != null ? modHead.features() : modHead.word();
+    future.add(word);
     return future;
   }
 
+  public boolean doCleanup() { return true; }
+
+  /**
+   * The trainer passes <code>ModifierEvent</code> objects
+   * representing modifiers on both sides of the head, even though
+   * there are separate models for each side.  This allows the final
+   * back-off level, p(w | t), to use information gathered from both
+   * sides.  However, it means that after all counts have been
+   * derived, there are twice as many entries in the counts tables for
+   * the back-off levels 0 and 1, since the model for, say, the left
+   * side will have stored counts for the right side as well (the
+   * counts are stored separately by explicitly including a "side"
+   * field in the events being counted).  This method allows for the
+   * removal of these "unnecessary" counts, which will never be used
+   * when decoding.
+   */
+  public boolean removeHistory(int backOffLevel, Event history) {
+    // this method assumes the "side" field will be the last element
+    // of the events in which it is included (at levels 0 and 1)
+    // N.B.: Right now, both levels 0 and 1 of both the regular and the
+    // baseNP models have a side field as their final element; if this
+    // changes, however, it will be necessary to check the parent
+    // field to see if the specified history is that for a baseNP
+    // and do a special baseNP case
+    Symbol side = (Symbol)additionalData;
+    switch (backOffLevel) {
+    case 0:
+      return history.get(0, history.numComponents(0) - 1) != side;
+    case 1:
+      return history.get(0, history.numComponents(0) - 1) != side;
+    case 2:
+      return false;
+    }
+    return false;
+  }
+
   public ProbabilityStructure copy() {
-    return new ModWordModelStructure2();
+    ProbabilityStructure psCopy = new ModWordModelStructure2();
+    psCopy.setAdditionalData(this.additionalData);
+    return psCopy;
   }
 }

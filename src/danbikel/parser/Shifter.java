51307eea9e7e74e91232810e157fab9307558ce0
dbikel <dbikel@b85a8ef5-4420-0410-ac20-a3093ec9fedb>
2002-06-26 15:53:35 +0000

    A class containing only static methods that mirror the signatures of the Shift interface, allowing a convenient flow-through mechanism to an internal static Shift object, the exact type of which is determined by the value of Settings.shifterClass.

---
+++ src/danbikel/parser/Shifter.java
@@ -0,0 +1,68 @@
+package danbikel.parser;
+
+import danbikel.util.*;
+import danbikel.lisp.*;
+
+/**
+ * A class containing only static methods that mirror the signatures of the
+ * {@link Shift} interface, allowing a convenient flow-through mechanism to
+ * an internal static {@link Shift} object, the exact type of which is
+ * determined by the value of {@link Settings#shifterClass}.
+ */
+public class Shifter {
+  private static final String className = Shifter.class.getName();
+  private static Shift shifter;
+
+  static {
+    String shifterStr= Settings.get(Settings.shifterClass);
+    if (shifterStr != null) {
+      try {
+	shifter = (Shift)Class.forName(shifterStr).newInstance();
+      }
+      catch (Exception e) {
+	System.err.println(className + ": error creating " +
+			   "instance of " + shifterStr + ":\n\t" + e +
+			   "\n\tusing DefaultShifter instead");
+	shifter = new DefaultShifter();
+      }
+    }
+    else {
+      System.err.println(className + ": error: the property " +
+			 Settings.shifterClass + " was not set;\n\t" +
+			 "using DefaultShifter");
+      shifter = new DefaultShifter();
+    }
+  }
+
+  public static void shift(TrainerEvent event, SexpList list,
+		    Sexp prevMod, Sexp currMod) {
+    shifter.shift(event, list, prevMod, currMod);
+  }
+  public static void shift(TrainerEvent event, WordList wordList,
+		    Word prevWord, Word currWord) {
+    shifter.shift(event, wordList, prevWord, currWord);
+  }
+
+  public static boolean skip(CKYItem item, Sexp prevMod, Sexp currMod) {
+    return shifter.skip(item, prevMod, currMod);
+  }
+  public static boolean skip(CKYItem item, Word prevWord, Word currWord) {
+    return shifter.skip(item, prevWord, currWord);
+  }
+
+
+
+
+  public static void shift(TrainerEvent event, SexpList list, Sexp sexp) {
+    shifter.shift(event, list, sexp);
+  }
+  public static void shift(TrainerEvent event, WordList wordList, Word word) {
+    shifter.shift(event, wordList, word);
+  }
+  public static boolean skip(CKYItem item, Sexp sexp) {
+    return shifter.skip(item, sexp);
+  }
+  public static boolean skip(CKYItem item, Word word) {
+    return shifter.skip(item, word);
+  }
+}

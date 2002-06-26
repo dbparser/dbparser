1e1d3847bead924bb8acedc1c1c4da1f9641bb50
dbikel <dbikel@b85a8ef5-4420-0410-ac20-a3093ec9fedb>
2002-06-26 18:00:23 +0000

    Added currently-unused side element to all histories constructed by getHistory method.  See javadoc comment for ModWordModelStructure2.removeHistory(int,Event) method to find out how and why the side field is used.

--- src/danbikel/parser/ModWordModelStructure3.java
+++ src/danbikel/parser/ModWordModelStructure3.java
@@ -12,7 +12,7 @@ public class ModWordModelStructure3 extends ProbabilityStructure {
     super();
   }
 
-  public int maxEventComponents() { return 9; }
+  public int maxEventComponents() { return 10; }
   public int numLevels() { return 3; }
 
   public Event getHistory(TrainerEvent trainerEvent, int backOffLevel) {
@@ -21,6 +21,8 @@ public class ModWordModelStructure3 extends ProbabilityStructure {
     if (modEvent.parent() == baseNP)
       return getBaseNPHistory(modEvent, backOffLevel);
 
+    Symbol side = Constants.sideToSym(modEvent.side());
+
     MutableEvent hist = historiesWithSubcats[backOffLevel];
     hist.clear();
     Symbol verbInterveningSym =
@@ -28,7 +30,7 @@ public class ModWordModelStructure3 extends ProbabilityStructure {
     switch (backOffLevel) {
     case 0:
       // for p(w_i | M(t)_i, P, H, w, t, verbIntervening, (M_i-1, ..., M_i-k),
-      //             subcat)
+      //             subcat, side)
       hist.add(0, Language.training.removeGapAugmentation(modEvent.modifier()));
       hist.add(0, modEvent.modHeadWord().tag());
       hist.add(0, Language.training.removeGapAugmentation(modEvent.parent()));
@@ -38,9 +40,10 @@ public class ModWordModelStructure3 extends ProbabilityStructure {
       hist.add(0, verbInterveningSym);
       hist.add(0, modEvent.previousMods());
       hist.add(1, modEvent.subcat());
+      hist.add(0, side);
       break;
     case 1:
-      // for p(w_i | M(t)_i, P, H, t, verbIntervening, map(M_i-1), subcat)
+      // for p(w_i | M(t)_i, P, H, t, verbIntervening, map(M_i-1), subcat, side)
       Symbol mappedPrevModSym =
 	Collins.mapPrevMod(modEvent.previousMods().symbolAt(0));
       hist.add(0, Language.training.removeGapAugmentation(modEvent.modifier()));
@@ -51,6 +54,7 @@ public class ModWordModelStructure3 extends ProbabilityStructure {
       hist.add(0, verbInterveningSym);
       hist.add(0, mappedPrevModSym);
       hist.add(1, modEvent.subcat());
+      hist.add(0, side);
       break;
     case 2:
       // for p(w_i | t_i)
@@ -64,6 +68,9 @@ public class ModWordModelStructure3 extends ProbabilityStructure {
 
   private Event getBaseNPHistory(ModifierEvent modEvent, int backOffLevel) {
     MutableEvent hist = histories[backOffLevel];
+
+    Symbol side = Constants.sideToSym(modEvent.side());
+
     Symbol prevModLabel =
       (modEvent.previousMods().get(0) == startSym ?
        modEvent.head() : modEvent.previousMods().symbolAt(0));
@@ -73,21 +80,23 @@ public class ModWordModelStructure3 extends ProbabilityStructure {
     hist.clear();
     switch (backOffLevel) {
     case 0:
-      // for p(w_i | M(t)_i, P, M(w,t)_i-1)
+      // for p(w_i | M(t)_i, P, M(w,t)_i-1, side)
       hist.add(Language.training.removeGapAugmentation(modEvent.modifier()));
       hist.add(modEvent.modHeadWord().tag());
       hist.add(Language.training.removeGapAugmentation(modEvent.parent()));
       hist.add(prevModLabel);
       hist.add(prevModWord.word());
       hist.add(prevModWord.tag());
+      hist.add(side);
       break;
     case 1:
-      // for p(w_i | M(t)_i, P, M(t)_i-1)
+      // for p(w_i | M(t)_i, P, M(t)_i-1, side)
       hist.add(Language.training.removeGapAugmentation(modEvent.modifier()));
       hist.add(modEvent.modHeadWord().tag());
       hist.add(Language.training.removeGapAugmentation(modEvent.parent()));
       hist.add(prevModLabel);
       hist.add(prevModWord.tag());
+      hist.add(side);
       break;
     case 2:
       // for p(w_i | t_i)

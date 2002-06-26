bbb357bae9f78872c753bdbc3d877738c98a5dc4
dbikel <dbikel@b85a8ef5-4420-0410-ac20-a3093ec9fedb>
2002-06-26 17:44:41 +0000

    Added a now-unnecessary side element to every event constructed by the getHistory method.

--- src/danbikel/parser/ModNonterminalModelStructure3.java
+++ src/danbikel/parser/ModNonterminalModelStructure3.java
@@ -23,7 +23,7 @@ public class ModNonterminalModelStructure3 extends ProbabilityStructure {
     super();
   }
 
-  public int maxEventComponents() { return 7; }
+  public int maxEventComponents() { return 8; }
   public int numLevels() { return 3; }
 
   public Event getHistory(TrainerEvent trainerEvent, int backOffLevel) {
@@ -32,6 +32,8 @@ public class ModNonterminalModelStructure3 extends ProbabilityStructure {
     if (modEvent.parent() == baseNP)
       return getBaseNPHistory(modEvent, backOffLevel);
 
+    Symbol side = Constants.sideToSym(modEvent.side());
+
     MutableEvent hist = historiesWithSubcats[backOffLevel];
 
     hist.clear();
@@ -39,7 +41,8 @@ public class ModNonterminalModelStructure3 extends ProbabilityStructure {
       Constants.booleanToSym(modEvent.verbIntervening());
     switch (backOffLevel) {
     case 0:
-      // for p(M(t)_i | P, H, w, t, verbIntervening, (M_i-1,...,M_i-k), subcat)
+      // for p(M(t)_i | P, H, w, t, verbIntervening, (M_i-1,...,M_i-k), subcat,
+      //                side)
       hist.add(0, Language.training.removeGapAugmentation(modEvent.parent()));
       hist.add(0, Language.training.removeGapAugmentation(modEvent.head()));
       hist.add(0, modEvent.headWord().word());
@@ -47,18 +50,20 @@ public class ModNonterminalModelStructure3 extends ProbabilityStructure {
       hist.add(0, verbInterveningSym);
       hist.add(0, Language.training.removeGapAugmentation(modEvent.previousMods()));
       hist.add(1, modEvent.subcat());
+      hist.add(0, side);
       break;
     case 1:
-      // for p(M(t)_i | P, H, t, verbIntervening, M_i-1, subcat)
+      // for p(M(t)_i | P, H, t, verbIntervening, M_i-1, subcat, side)
       hist.add(0, Language.training.removeGapAugmentation(modEvent.parent()));
       hist.add(0, Language.training.removeGapAugmentation(modEvent.head()));
       hist.add(0, modEvent.headWord().tag());
       hist.add(0, verbInterveningSym);
       hist.add(0, Language.training.removeGapAugmentation(modEvent.previousMods().get(0)));
       hist.add(1, modEvent.subcat());
+      hist.add(0, side);
       break;
     case 2:
-      // for p(M(t)_i | P, H, verbIntervening, map(M_i-1), subcat)
+      // for p(M(t)_i | P, H, verbIntervening, map(M_i-1), subcat, side)
       Symbol mappedPrevMod =
 	Collins.mapPrevMod(modEvent.previousMods().symbolAt(0));
       hist.add(0, Language.training.removeGapAugmentation(modEvent.parent()));
@@ -66,6 +71,7 @@ public class ModNonterminalModelStructure3 extends ProbabilityStructure {
       hist.add(0, verbInterveningSym);
       hist.add(0, mappedPrevMod);
       hist.add(1, modEvent.subcat());
+      hist.add(0, side);
       break;
     }
     return hist;
@@ -73,6 +79,9 @@ public class ModNonterminalModelStructure3 extends ProbabilityStructure {
 
   private Event getBaseNPHistory(ModifierEvent modEvent, int backOffLevel) {
     MutableEvent hist = histories[backOffLevel];
+
+    Symbol side = Constants.sideToSym(modEvent.side());
+
     Symbol prevModLabel =
       (modEvent.previousMods().get(0) == startSym ?
        modEvent.head() : modEvent.previousMods().symbolAt(0));
@@ -82,22 +91,25 @@ public class ModNonterminalModelStructure3 extends ProbabilityStructure {
     hist.clear();
     switch (backOffLevel) {
     case 0:
-      // for p(M(t)_i | P, M(w,t)_i-1)
+      // for p(M(t)_i | P, M(w,t)_i-1, side)
       hist.add(Language.training.removeGapAugmentation(modEvent.parent()));
       hist.add(prevModLabel);
       hist.add(prevModWord.word());
       hist.add(prevModWord.tag());
+      hist.add(side);
       break;
     case 1:
-      // for p(M(t)_i | P, M(t)_i-1)
+      // for p(M(t)_i | P, M(t)_i-1, side)
       hist.add(Language.training.removeGapAugmentation(modEvent.parent()));
       hist.add(prevModLabel);
       hist.add(prevModWord.tag());
+      hist.add(side);
       break;
     case 2:
-      // for p(M(t)_i | P, M_i-1)
+      // for p(M(t)_i | P, M_i-1, side)
       hist.add(Language.training.removeGapAugmentation(modEvent.parent()));
       hist.add(prevModLabel);
+      hist.add(side);
       break;
     }
     return hist;

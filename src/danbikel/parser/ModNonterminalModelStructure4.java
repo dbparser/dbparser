198a6beaff4dd628e1d614df6c8f70f8be60ec3f
dbikel <dbikel@b85a8ef5-4420-0410-ac20-a3093ec9fedb>
2002-09-04 18:20:26 +0000

    Identical to model in ModNonterminalModelStructure2.java, with the addition of a fourth back-off level, p(M(t) | P), i.e., where the parent nonterminal is the only element of the history context. This is a much "looser" model, created to allow the E-step of an EM run to "hallucinate" many more derivations for a given labeled bracketing.

---
+++ src/danbikel/parser/ModNonterminalModelStructure4.java
@@ -0,0 +1,171 @@
+package danbikel.parser;
+
+import danbikel.lisp.*;
+
+/**
+ * Representation of the complete back-off structure of the generation model
+ * for modifying nonterminals/part-of-speech tags (the modifying nonterminals
+ * are partially lexicalized with the parts of speech of their respective
+ * head words).
+ * <p>
+ * <b>It is a horrendous bug that all of these <code>ProbabilityStructure</code>
+ * classes do not copy various lists from the <code>TrainerEvent</code> objects
+ * before removing gap augmentations from their elements.</b>
+ * <p>
+ */
+public class ModNonterminalModelStructure4 extends ProbabilityStructure {
+  // data members
+  private static Symbol startSym = Language.training().startSym();
+  private static Word startWord = Language.training().startWord();
+  private static Symbol baseNP = Language.treebank().baseNPLabel();
+  private Symbol topSym = Language.training().topSym();
+
+  public ModNonterminalModelStructure4() {
+    super();
+  }
+
+  public int maxEventComponents() { return 8; }
+  public int numLevels() { return 4; }
+
+  public Event getHistory(TrainerEvent trainerEvent, int backOffLevel) {
+    ModifierEvent modEvent = (ModifierEvent)trainerEvent;
+
+    if (modEvent.parent() == baseNP)
+      return getBaseNPHistory(modEvent, backOffLevel);
+
+    Symbol side = Constants.sideToSym(modEvent.side());
+
+    MutableEvent hist = historiesWithSubcats[backOffLevel];
+
+    hist.clear();
+    Symbol verbInterveningSym =
+      Constants.booleanToSym(modEvent.verbIntervening());
+    Symbol mappedPrevModSym =
+      Collins.mapPrevMod(modEvent.previousMods().symbolAt(0));
+    Symbol parent =
+      Language.training.removeArgAugmentation(modEvent.parent());
+
+    switch (backOffLevel) {
+    case 0:
+      // for p(M(t)_i | P, H, w, t, verbIntervening, map(M_i-1), subcat, side)
+      hist.add(0, parent);
+      hist.add(0, Language.training.removeGapAugmentation(modEvent.head()));
+      hist.add(0, modEvent.headWord().word());
+      hist.add(0, modEvent.headWord().tag());
+      hist.add(0, verbInterveningSym);
+      hist.add(0, mappedPrevModSym);
+      hist.add(1, modEvent.subcat());
+      hist.add(0, side);
+      break;
+    case 1:
+      // for p(M(t)_i | P, H, t, verbIntervening, map(M_i-1), subcat, side)
+      hist.add(0, parent);
+      hist.add(0, Language.training.removeGapAugmentation(modEvent.head()));
+      hist.add(0, modEvent.headWord().tag());
+      hist.add(0, verbInterveningSym);
+      hist.add(0, mappedPrevModSym);
+      hist.add(1, modEvent.subcat());
+      hist.add(0, side);
+      break;
+    case 2:
+      // for p(M(t)_i | P, H, verbIntervening, map(M_i-1), subcat, side)
+      hist.add(0, parent);
+      hist.add(0, Language.training.removeGapAugmentation(modEvent.head()));
+      hist.add(0, verbInterveningSym);
+      hist.add(0, mappedPrevModSym);
+      hist.add(1, modEvent.subcat());
+      hist.add(0, side);
+      break;
+    case 3:
+      // for p(M(t)_i | P)
+      hist = histories[backOffLevel]; // efficiency hack: don't need subcat
+      hist.clear();
+      hist.add(0, parent);
+      break;
+    }
+    return hist;
+  }
+
+  private Event getBaseNPHistory(ModifierEvent modEvent, int backOffLevel) {
+    MutableEvent hist = histories[backOffLevel];
+
+    Symbol side = Constants.sideToSym(modEvent.side());
+
+    Symbol prevModLabel =
+      (modEvent.previousMods().get(0) == startSym ?
+       modEvent.head() : modEvent.previousMods().symbolAt(0));
+    Word prevModWord =
+      (modEvent.previousWords().getWord(0).equals(startWord) ?
+       modEvent.headWord() : modEvent.previousWords().getWord(0));
+    hist.clear();
+    switch (backOffLevel) {
+    case 0:
+      // for p(M(t)_i | P, M(w,t)_i-1, side)
+      hist.add(Language.training.removeGapAugmentation(modEvent.parent()));
+      hist.add(prevModLabel);
+      hist.add(prevModWord.word());
+      hist.add(prevModWord.tag());
+      hist.add(side);
+      break;
+    case 1:
+      // for p(M(t)_i | P, M(t)_i-1, side)
+      hist.add(Language.training.removeGapAugmentation(modEvent.parent()));
+      hist.add(prevModLabel);
+      hist.add(prevModWord.tag());
+      hist.add(side);
+      break;
+    case 2:
+      // for p(M(t)_i | P, M_i-1, side)
+      hist.add(Language.training.removeGapAugmentation(modEvent.parent()));
+      hist.add(prevModLabel);
+      hist.add(side);
+      break;
+    case 3:
+      // for p(M(t)_i | P, M_i-1, side)
+      hist.add(Language.training.removeGapAugmentation(modEvent.parent()));
+      hist.add(prevModLabel);
+      hist.add(side);
+      break;
+    }
+    return hist;
+  }
+
+  public Event getFuture(TrainerEvent trainerEvent, int backOffLevel) {
+    ModifierEvent modEvent = (ModifierEvent)trainerEvent;
+    MutableEvent future = futures[backOffLevel];
+    future.clear();
+    future.add(modEvent.modifier());
+    future.add(modEvent.modHeadWord().tag());
+    return future;
+  }
+
+  public boolean doCleanup() { return true; }
+
+  /**
+   * In order to gather statistics for words that appear as the head of
+   * the entire sentence when estimating p(w | t), the trainer "fakes" a
+   * modifier event, as though the root node of the observed tree was seen
+   * to modify the magical +TOP+ node.  For back-off levels 0 and 1, we
+   * will never use the derived counts whose history contexts contain +TOP+.
+   * This method allows for the removal of these "unnecessary" counts,
+   * which will never be used when decoding.
+   */
+  public boolean removeHistory(int backOffLevel, Event history) {
+    // this method assumes the parent component of histories for
+    // back-off levels 0 and 1 will be at index 0.  IF THIS CHANGES,
+    // this method will need to change accordingly.
+    switch (backOffLevel) {
+    case 0:
+      return history.get(0, 0) == topSym;
+    case 1:
+      return history.get(0, 0) == topSym;
+    case 2:
+      return false;
+    }
+    return false;
+  }
+
+  public ProbabilityStructure copy() {
+    return new ModNonterminalModelStructure4();
+  }
+}

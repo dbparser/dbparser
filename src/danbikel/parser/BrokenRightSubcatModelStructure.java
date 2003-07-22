7dc279c8ae2b167e1854ca095144f97bf268ec92
dbikel <dbikel@b85a8ef5-4420-0410-ac20-a3093ec9fedb>
2003-07-22 19:34:20 +0000

    Broken version to emulate clean-room implementation of Mike Collins' thesis parser.

---
+++ src/danbikel/parser/BrokenRightSubcatModelStructure.java
@@ -0,0 +1,24 @@
+package danbikel.parser;
+
+import danbikel.lisp.*;
+
+/**
+ * Representation of the complete back-off structure of the subcat-generation
+ * model for the right side of the head child.
+ */
+public class BrokenRightSubcatModelStructure extends SubcatModelStructure1 {
+  public BrokenRightSubcatModelStructure() {
+    super();
+  }
+
+  public double lambdaFudge(int backOffLevel) { return 5.0; }
+  public double lambdaFudgeTerm(int backOffLevel) { return 0.0; }
+
+  public Event getFuture(TrainerEvent trainerEvent, int backOffLevel) {
+    return ((HeadEvent)trainerEvent).rightSubcat();
+  }
+
+  public ProbabilityStructure copy() {
+    return new BrokenRightSubcatModelStructure();
+  }
+}

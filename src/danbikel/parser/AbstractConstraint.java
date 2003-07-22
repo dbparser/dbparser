210641275dda37c6d7bf83b23d1ce06f484b82a9
dbikel <dbikel@b85a8ef5-4420-0410-ac20-a3093ec9fedb>
2003-07-22 19:33:42 +0000

    Abstract base class to make it easy to implement the Constraint interface.

---
+++ src/danbikel/parser/AbstractConstraint.java
@@ -0,0 +1,33 @@
+package danbikel.parser;
+
+/**
+ * An abstract base class that throws an
+ * <code>UnsupportedOperationException</code> for every optional operation
+ * of the <code>Constraint</code> interface.
+ */
+public class AbstractConstraint implements Constraint {
+
+  protected AbstractConstraint() {
+  }
+  public boolean isLeaf() {
+    throw new java.lang.UnsupportedOperationException();
+  }
+  public Constraint getParent() {
+    throw new java.lang.UnsupportedOperationException();
+  }
+  public boolean isSatisfiedBy(Item item) {
+    throw new java.lang.UnsupportedOperationException();
+  }
+  public boolean hasBeenSatisfied() {
+    throw new java.lang.UnsupportedOperationException();
+  }
+  public boolean isLocallySatisfiedBy(Item item) {
+    throw new java.lang.UnsupportedOperationException();
+  }
+  public boolean isViolatedBy(Item item) {
+    throw new java.lang.UnsupportedOperationException();
+  }
+  public boolean isViolatedByChild(Item childItem) {
+    throw new java.lang.UnsupportedOperationException();
+  }
+}

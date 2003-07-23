b28dda97184051aea81374af8b588ebca80e3a86
dbikel <dbikel@b85a8ef5-4420-0410-ac20-a3093ec9fedb>
2003-07-23 19:26:01 +0000

    A model of generating partially-lexicalized modifying nonterminals.

---
+++ src/danbikel/parser/ModNonterminalModelStructure5.java
@@ -0,0 +1,40 @@
+package danbikel.parser;
+
+/**
+ * This class provides a probability structure identical to its superclass,
+ * except that {@link #getFuture(TrainerEvent,int)} has been overridden so
+ * that the future only consists of an unlexicalized modifying nonterminal
+ * label.  Furthermore, this probability structure overrides the
+ * {@link #jointModel} method to return an array containing a
+ * {@link TagModelStructure1} instance.
+ *
+ * @see #jointModel
+ * @see TagModelStructure1
+ */
+public class ModNonterminalModelStructure5
+  extends ModNonterminalModelStructure4 {
+
+  private ProbabilityStructure[] jointStructures = {new TagModelStructure1()};
+
+  public ModNonterminalModelStructure5() {
+    super();
+  }
+
+  public Event getFuture(TrainerEvent trainerEvent, int backOffLevel) {
+    ModifierEvent modEvent = (ModifierEvent)trainerEvent;
+    MutableEvent future = futures[backOffLevel];
+    future.clear();
+    future.add(modEvent.modifier());
+    return future;
+  }
+
+  public Model newModel() { return new JointModel(this); }
+
+  public ProbabilityStructure[] jointModel() {
+    return jointStructures;
+  }
+
+  public ProbabilityStructure copy() {
+    return new ModNonterminalModelStructure5();
+  }
+}

24bb9329f035204fabf3bea2cf53bac4273ad9c0
dbikel <dbikel@b85a8ef5-4420-0410-ac20-a3093ec9fedb>
2003-07-23 20:51:38 +0000

    Stores a set of PartialLexTreeConstraint objects, and provides methods required by the ConstraintSet interface.

---
+++ src/danbikel/parser/PartialLexTreeConstraintSet.java
@@ -0,0 +1,48 @@
+package danbikel.parser;
+
+import danbikel.lisp.*;
+import java.util.*;
+
+public class PartialLexTreeConstraintSet extends UnlexTreeConstraintSet {
+
+  public PartialLexTreeConstraintSet() {
+    super();
+  }
+
+  public PartialLexTreeConstraintSet(Sexp tree) {
+    super(tree);
+  }
+
+  /**
+   * Builds the constraint tree from the specified unlexicalized parse tree.
+   * As a necessary side-effect, the {@link #root} and {@link #leaves} data
+   * members will be set/populated by this method.
+   *
+   * @param tree the tree from which to build this constraint set
+   */
+  protected void buildConstraintSet(Sexp tree) {
+    root = new PartialLexTreeConstraint(tree);
+    collectNodes(root);
+    list.trimToSize();
+    leaves.trimToSize();
+  }
+
+  /**
+   * Test driver for this class.
+   * @param args
+   */
+  public static void main(String[] args) {
+    try {
+      SexpTokenizer tok =
+	new SexpTokenizer(System.in, Language.encoding(), 8192);
+      Sexp curr = null;
+      while ((curr = Sexp.read(tok)) != null) {
+	PartialLexTreeConstraintSet set = new PartialLexTreeConstraintSet(curr);
+	System.out.println(((SexpConvertible)set.root()).toSexp());
+      }
+    }
+    catch (Exception e) {
+      System.err.println(e);
+    }
+  }
+}

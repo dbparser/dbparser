86fb7242566dafb94ce175c7fe02373ccf92bed7
dbikel <dbikel@b85a8ef5-4420-0410-ac20-a3093ec9fedb>
2003-08-21 17:58:49 +0000

    All the required language package classes have now been changed to interfaces with partial default implementations, in the form of abstract base classes in the new package danbikel.parser.lang.
    This new abstract base class used to be danbikel.parser.WordFeatures, but that
    is now an interface.

---
+++ src/danbikel/parser/lang/AbstractWordFeatures.java
@@ -0,0 +1,45 @@
+package danbikel.parser.lang;
+
+import danbikel.lisp.*;
+import danbikel.parser.*;
+import java.io.Serializable;
+
+public abstract class AbstractWordFeatures implements WordFeatures, Serializable {
+  /**
+   * The unique symbol to represent unknown words.  The default value
+   * is the return value of <code>Symbol.add(&quot;+unknown+&quot;)</code>;
+   * if this maps to an actual word in a particular language or Treebank,
+   * this data member should be reassigned in a subclass.
+   */
+  protected static Symbol unknownWordSym = Symbol.add("+unknown+");
+
+  /**
+   * Default constructor, to be called by subclasses (usually implicitly).
+   */
+  protected AbstractWordFeatures() {
+  }
+
+  /**
+   * Returns a symbol representing the orthographic and/or morphological
+   * features of the specified word. This default implementation simply returns
+   * the unknown word symbol.
+   *
+   * @param word the word whose features are to be computed
+   * @param firstWord whether <code>word</code> is the first word in the
+   * sentence (useful when computing capitalization features for certain
+   * languages, such as English)
+   * @return a symbol representing the orthographic and/or morphological
+   * features of <code>word</code>
+   *
+   * @see #unknownWordSym
+   */
+  public Symbol features(Symbol word, boolean firstWord) {
+    return unknownWordSym;
+  }
+
+  /**
+   * The symbol that represents the case where none of the features fires
+   * for a particular word.
+   */
+  public abstract Symbol defaultFeatureVector();
+}

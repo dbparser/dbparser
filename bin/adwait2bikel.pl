60ab1b892b670ef60e85d575ad5c99c84722c542
dbikel <dbikel@b85a8ef5-4420-0410-ac20-a3093ec9fedb>
2004-03-03 20:54:08 +0000

    Fixed bug.

--- bin/adwait2bikel.pl
+++ bin/adwait2bikel.pl
@@ -3,7 +3,7 @@
 # converts Adwait's tagged format to ours
 # Adwait's format is one sentence per line, with each sentence having the 
 # form
-*   word1_tag1 word2_tag2
+#   word1_tag1 word2_tag2
 
 while (<>) {
     s/[()]//g;

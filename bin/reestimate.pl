09d5874489310b89ef1110eb8659dd3378764aee
dbikel <dbikel@b85a8ef5-4420-0410-ac20-a3093ec9fedb>
2004-04-13 20:03:50 +0000

    Many changes making this script more flexible as well as more robust.

--- bin/reestimate.pl
+++ bin/reestimate.pl
@@ -3,7 +3,12 @@
 use Getopt::Long;
 
 $iterations = 20;
-GetOptions("i|iterations=i" => \$iterations) or usage();
+$settingsFile = $ENV{"HOME"} . "/jbproject/dbparser/settings/em.properties";
+$trainingData =
+    $ENV{"HOME"} . "/scratch/wsj-02-21.mrg";
+GetOptions("i|iterations=i" => \$iterations,
+	   "-sf|settings-file=s" => \$settingsFile,
+	   "-t=s" => \$trainingData) or usage();
 
 if (@ARGV == 1) {
     $expDir = $ARGV[0]
@@ -18,9 +23,13 @@ chdir $expDir or die "Can't cd to $expDir: $!\n";
 print "Performing $iterations iteration", ($iterations == 1 ? "" : "s"), ".\n";
 
 # data that won't change from iteration to iteration
-$trainingData = $ENV{"HOME"} . "/scratch/wsj-02-21.mrg";
-$java = "java";
-@mem = ("-Xms1600m", "-Xmx1600m");
+# grab tail root and tail extension of $trainingData
+# (we don't currently use the extension)
+($path, $root, $extension) = $trainingData =~ m/(.*\/)?(.*)\.(.*)/;
+@java = ("java");
+@javaServer = ("java", "-server");
+$settings = "-Dparser.settingsFile=$settingsFile";
+@mem = ("-Xms1800m", "-Xmx1800m");
 $emParserClass = "danbikel.parser.EMParser";
 $trainerClass = "danbikel.parser.Trainer";
 
@@ -34,10 +43,10 @@ if (@dirs == 0) {
     exit(1);
 }
 
-$staticData = "$dirs[0]/wsj-02-21.static.observed";
+$staticData = "$dirs[0]/" . $root . ".static.observed.gz";
 if (! -f $staticData) {
     # try to create static data from first observed file
-    $firstObservations = "$dirs[0]/wsj-02-21.observed";
+    $firstObservations = "$dirs[0]/" . $root . ".observed.gz";
     if (! -f $firstObservations) {
 	print STDERR
 	    "Couldn't find static data file \"$staticData\".\n",
@@ -48,10 +57,10 @@ if (! -f $staticData) {
     print STDERR
 	"Creating static data file\n\t\"$staticData\"\nfrom ",
 	"initial observations file\n\t\"$firstObservations\".\n";
-    open FIRSTOBS, $firstObservations;
-    open STATIC, ">$staticData";
+    open FIRSTOBS, "gunzip -c $firstObservations |";
+    open STATIC, "| gzip -c -9 > $staticData";
     while (<FIRSTOBS>) {
-	if (! m/^\((mod |head )/) {
+	if (! m/^\((mod |head |gap )/) {
 	    print STATIC $_;
 	}
     }
@@ -67,31 +76,52 @@ for ($i = 0; $i < $iterations; $i++) {
 
     mkdir($nextDir, 0775);
 
-    $currentModel = "$prevDir/wsj-02-21.obj";
-    $newCounts = "$nextDir/wsj-02-21.observed";
-    $newModel = "$nextDir/wsj-02-21.obj";
+    $currentModel = "$prevDir/" . $root . ".obj.gz";
+    $newCounts = "$nextDir/" . $root . ".observed.gz";
+    $newModel = "$nextDir/" . $root . ".obj.gz";
 
     # first, use EMParser to constrain-parse using current model
-    system($java, @mem, $emParserClass, "-is",  $currentModel,
+    @smoothingSettings =
+	("-Dparser.model.smoothingParametersDir=$dirs[0]/smoothing",
+	 "-Dparser.model.useSmoothingParameters=true");
+    print join(" ", (@javaServer, $settings, @smoothingSettings, @mem,
+		     $emParserClass, "-is", $currentModel,
+		     "-sa", $trainingData, "-out", $newCounts)),
+          "\n";
+    system(@javaServer, $settings, @smoothingSettings, @mem,
+	   $emParserClass, "-is",  $currentModel,
 	   "-sa", $trainingData, "-out", $newCounts);
 
     # next, append static counts to new counts file
-    open NEWCOUNTS, ">>$newCounts";
-    open STATIC, $staticData;
-    while (<STATIC>) {
-	print NEWCOUNTS $_;
-    }
-    close NEWCOUNTS;
-    close STATIC;
+#     open NEWCOUNTS, ">>$newCounts";
+#     open STATIC, $staticData;
+#     while (<STATIC>) {
+# 	print NEWCOUNTS $_;
+#     }
+#     close NEWCOUNTS;
+#     close STATIC;
 
     # finally, use Trainer to compute derived counts
-    system($java, @mem, $trainerClass, "-l", $newCounts, "-od", $newModel);
+    print join(" ", (@java, $settings, @smoothingSettings, @mem,
+		     $trainerClass, "-it", "-l", $staticData, "-l", $newCounts,
+		     "-od", $newModel)),
+          "\n";
+    system(@java, $settings, @smoothingSettings, @mem,
+	   $trainerClass, "-it", "-l", $staticData, "-l", $newCounts,
+	   "-od", $newModel);
 
     push @dirs, $nextDir;
 }
 
 sub usage {
     print STDERR
-	"usage: [-i|--iterations <num iterations>] <experiment directory>\n";
+	"usage: [-i|--iterations <num iterations>] ",
+	"[-sf|--settings-file <em settings>]\n\t[-t <training file>] ",
+	"<experiment directory>\n";
+    print STDERR
+	"defaults:\n",
+	"\t<num iterations>=$iterations\n",
+	"\t<em settings>=$settingsFile\n",
+	"\t<training file>=$trainingData\n";
     exit(1);
 }

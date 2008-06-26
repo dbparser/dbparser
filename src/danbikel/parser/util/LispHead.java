318ba1c4b244f1708c8dacec630bafe2f240c0d2
dbikel <dbikel@b85a8ef5-4420-0410-ac20-a3093ec9fedb>
2008-06-26 22:34:31 +0000

    * Fixed javadoc error in a comment in ProbabilityStructure.
    * Added a new option, -i, to LispHead.main.  If present, it means that the
      input file will be expected to contain IBM-format parse trees (the default,
      of course, is to expect Penn Treebank II-style trees).

--- src/danbikel/parser/util/LispHead.java
+++ src/danbikel/parser/util/LispHead.java
@@ -17,15 +17,17 @@ public class LispHead {
   private LispHead() {}
 
   private static void usage() {
-    System.err.println("usage: [-number | -n number ] [ filename ]");
+    System.err.println("usage: [-i] [-number | -n number ] [ filename ]");
   }
 
   /**
    * Prints out the first <tt>n</tt> S-expressions from a file or standard
-   * input.
-   * <pre>usage: [-number | -n number ] [ filename ]</pre>
+   * input.  Using the <tt>-i</tt> option indicates that the tree file being
+   * read is in IBM format.
+   * <pre>usage: [-i] [-number | -n number ] [ filename ]</pre>
    */
   public static void main(String[] args) {
+    boolean ibmFormat = false;
     int numSexps = 10;
     String numSexpsStr = null;
     InputStream inStream = System.in;
@@ -39,7 +41,10 @@ public class LispHead {
 	  else
 	    numSexpsStr = args[++i];
 	}
-	else
+        else if (args[i].equals("-i")) {
+          ibmFormat = true;
+        }
+        else
 	  numSexpsStr = args[i].substring(1);
       }
       else
@@ -64,10 +69,12 @@ public class LispHead {
         new OutputStreamWriter(System.out, enc);
       PrintWriter out =
         new PrintWriter(new BufferedWriter(outStreamWriter, bufSize));
-      SexpTokenizer tok = new SexpTokenizer(inStream, enc, bufSize);
+      SexpTokenizer tok = ibmFormat ?
+                          Util.ibmTokenizer(newReader(inStream, enc), true) :
+                          new SexpTokenizer(inStream, enc, bufSize);
       Sexp curr = null;
       try {
-        while (numSexps-- > 0 && (curr = Sexp.read(tok)) != null) {
+        while (numSexps-- > 0 && (curr = readTree(tok, ibmFormat)) != null) {
   	out.println(curr.toString());
         }
       } catch (IOException ioe) { System.err.println(ioe); }
@@ -76,5 +83,15 @@ public class LispHead {
     catch (UnsupportedEncodingException uee) {
       System.err.println(uee);
     }
+  }
+
+  private static BufferedReader newReader(InputStream inStream, String enc)
+    throws UnsupportedEncodingException {
+    return new BufferedReader(new InputStreamReader(inStream, enc), bufSize);
+  }
+
+  private static Sexp readTree(SexpTokenizer tok, boolean ibmFormat)
+    throws IOException {
+    return ibmFormat ? Util.readIbmTree(tok) : Sexp.read(tok);
   }
 }
